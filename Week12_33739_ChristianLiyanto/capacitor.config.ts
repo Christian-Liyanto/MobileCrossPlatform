44c0d3281d78e3f152d6481525cec4520c3b3c32
Christian-Liyanto <christian.liyanto@student.umn.ac.id>
2022-05-17 14:34:26 +0700

    Week 12

---
+++ Week12_33739_ChristianLiyanto/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Week12_33739_ChristianLiyanto',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
